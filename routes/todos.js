6652816901edb9b7004686747c20d613f1eed26e
thudsonbu <hudson_thom@bentley.edu>
2020-07-29 20:10:58 +0000

    Refactored route callback functions to helpers file

--- routes/todos.js
+++ routes/todos.js
@@ -1,62 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
+var helpers = require('../helpers/todos');
 
-// get all todos
-router.get('/', function(req, res){
-	db.Todo.find()
-	.then(function(todos){
-		res.json(todos);
-	})
-	.catch(function(err){
-		res.send(err);
-	})
-}); 
-
-// create todos
-router.post('/', function(req, res){
-	db.Todo.create(req.body)
-	.then(function(newTodo){
-		res.status(201).json(newTodo);
-	})
-	.catch(function(err){
-		res.send(err);
-	})
-});
+// root route
+router.route('/')
+	// get all todos	
+	.get(helpers.getTodos)
+	// create todo
+	.post(helpers.createTodo)
 
 // get individual todos
-router.get('/:todoId', function(req, res){
-	db.Todo.findById(req.params.todoId)
-	.then(function(foundTodo){
-		res.json(foundTodo);
-	})
-	.catch(function(err){
-		res.send(err);
-	})
-})
-
-// update
-router.put('/:todoId', function(req, res){
-	db.Todo.findOneAndUpdate({ _id: req.params.todoId}, req.body, {new: true})
-	.then(function(todo){
-		res.json(todo);
-	})
-	.catch(function(err){
-		res.send(err);
-	})
-})
-
-// delete
-router.delete('/:todoId', function(req, res){
-	db.Todo.deleteOne({_id: req.params.todoId})
-	.then(function(todo){
-		res.json({ message: "To do deleted"});
-	})
-	.catch(function(err){
-		res.send(err);
-	})
-})
-
+router.route('/:todoId')
+	// get todo
+	.get(helpers.getTodo)
+	// update todo
+	.put(helpers.updateTodo)
+	// delete todo
+	.delete(helpers.deleteTodo)
 
 
 module.exports = router;
