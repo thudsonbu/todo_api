d9fddd565eeb306734d3e5d5f69dd72370c8e484
thudsonbu <hudson_thom@bentley.edu>
2020-08-02 00:21:56 +0000

    Added static html page for todos, styling, and on page jquerry

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/api/todos', todoRoutes);
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 
 app.get('/', function(req, res){
-	res.send("Hello From Root route");
+	res.sendFile("index.html");
 });
 
+app.use('/api/todos', todoRoutes);
 
 app.listen(3000, function() { 
   console.log('Todo API listening on port 3000.'); 
